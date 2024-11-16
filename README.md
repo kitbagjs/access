5e6a69f7e13ad8f99ad6a5ff1cb8b720f45d1fb2
Evan Sutherland <stackoverfloweth@gmail.com>
2024-11-16 12:53:58 -0600

    Core file pack added

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# access-kit
+# @kitbag/access-kit
+
 Type safe attribute-based access control flow
+
+[![NPM Version][npm-badge]][npm-url]
+[![Netlify Status][netlify-badge]][netlify-url]
+[![Discord chat][discord-badge]][discord-url]
+
+## Getting Started
+
+Get started with the [documentation](https://kitbag-access-kit.netlify.app/)
+
+## Installation
+
+```bash
+# bun
+bun add @kitbag/access-kit
+# yarn
+yarn add @kitbag/access-kit
+# npm
+npm install @kitbag/access-kit
+```
+
+[npm-badge]: https://img.shields.io/npm/v/@kitbag/access-kit.svg
+[npm-url]: https://www.npmjs.org/package/@kitbag/access-kit
+[netlify-badge]: https://api.netlify.com/api/v1/badges/f003aad4-3c51-4830-bb77-8f8fd3b67b58/deploy-status
+[netlify-url]: https://app.netlify.com/sites/kitbag-access-kit/deploys
+[discord-badge]: https://img.shields.io/discord/1079625926024900739?logo=discord&label=Discord
+[discord-url]: https://discord.gg/zw7dpcc5HV
---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  test: {
+    typecheck: {
+      checker: 'vue-tsc',
+      ignoreSourceErrors: true,
+      include: ['src/**/*.spec-d.ts'],
+    },
+    include: ['src/**/*.spec.ts'],
+    environmentMatchGlobs: [
+      ['**\/*.browser.spec.ts', 'happy-dom'],
+      ['**\/*.spec.ts', 'node'],
+    ],
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src'),
+      },
+    ],
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: '@kitbag/access-kit',
+      fileName: 'kitbag-access-kit',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    dts({
+      rollupTypes: true,
+    }),
+  ],
+})
